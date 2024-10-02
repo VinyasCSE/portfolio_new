7a9041f767f174786d3f3665850e17f07793ebdc
pratyushcse <pratyushsuvarna4@gmail.com>
2024-10-02 21:14:58 +0530

    first commit

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { BsLinkedin } from 'react-icons/bs';
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+import './testimonials.css';
+
+const Testimonials = () => {
+  const testimonials = [
+    {
+      id: 1,
+      link: '#',
+      name: 'Sneha',
+      role: 'Cyber Security Engineer . Improving open-source projects, one commit at a time.',
+      test: 'As a developer and a problem solver, I think kavana is a great collaborative partner to have. I met kavana in some basic javascript & react projects and since then he has drastically progressed in him understanding of the development process. He always has a professional environment and has good audio and video quality which makes it easier to communicate with him.',
+    },
+    {
+      id: 2,
+      link: '',
+      name: 'Shaina',
+      role: 'Full-stack developer | Technical Support Engineer at Microverse',
+      test: "It was a pleasure collaborating with kavana on different projects. One of the things that I think is special about him is that he never settles, even after completing the projects he finds other resources and strengthens him knowledge. Collaborating with him is easy and comfortable, it's like working with someone you've known for a long period of time.",
+    },
+    {
+      id: 3,
+      link: '',
+      name: 'Vidya',
+      role: 'Full Stack Developer| PostgreSQL | JavaScript | React | Redux | Html&Css | Python.',
+      test: 'I worked with kavana in the same team and him communication skills are very strong. Her programming skill is one of the bests, given the time frame in the field. He is a good team player.  He will probably fit in most of the companies not only because he\'s a good team worker, but also because he has very good skills and I know he has much more potential to be shown.',
+    },
+    {
+      id: 4,
+      link: '#',
+      name: 'Manjusha',
+      role: 'Full-Stack Web Developer. JavaScript | Rails | React | Redux.',
+      test: "I mentored  some months ago, and I can say that he is one of those people that you love to work with. He was always interested in what I was trying to teach him, paying attention and always asking questions if something was not clear. When it comes to professional skills, he is really committed to work, always doing him best and going beyond the requirements of the project he's building.",
+    },
+    
+  ];
+  return (
+    <section id="testmonials">
+      <h5>Feedback from my peers</h5>
+      <h2>Testimonials</h2>
+      <Swiper 
+        className="container testimonials__container"
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        >
+        {testimonials.map((test) => (
+          <SwiperSlide className="testimonial" key={test.id}>
+          <div className="client__avatar">
+            <a href={test.link}>
+              <BsLinkedin />
+            </a>
+          </div>
+          <h5 className='client__name'>{test.name}</h5>
+          <small className="client__review">{test.test}</small>
+        </SwiperSlide>
+        ))}
+      </Swiper>
+    </section>
+  )
+}
+
+export default Testimonials
